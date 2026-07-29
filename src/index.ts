b35230fade3a8357b546d6fc0ca00535f80ac098
alnaga-dotcom <alnaga@gmail.com>
2026-07-29 07:07:31 +0300

    feat: expose Factory v1 core modules

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export * from "./domain/contracts.js";
+export * from "./ease/normalize.js";
+export * from "./routing/model-router.js";
+export * from "./qa/policy.js";
+export * from "./providers/video-provider.js";
